c3aad93607c064fb71a930cef865a22f6e43f47d
Yuleidy Valladares <yv@plusteam.tech>
2020-07-29 11:58:36 -0400

    🍎 Add Cypress and Vuetify
    - Add vuetify
    - Add cypress plugin for vue
    - Add basic test to HelloWorld component
    - Create info card to add tests for it

---
+++ src/components/InfoCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="mx-auto" max-width="344">
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text>{{ text }}</v-card-text>
+    <v-card-actions>
+      <v-btn @click="action">{{ buttonText }}</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class InfoCard extends Vue {
+  @Prop({ type: String }) title!: string;
+  @Prop({ type: String }) text!: string;
+  @Prop({ type: String }) buttonText!: string;
+
+  action(): void {
+    this.$emit("action");
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
